2a7f83493b50e4d7c37ed30775b4ed0a1feb26bd
MikolajAmbroziak2017 <35327147+MikolajAmbroziak2017@users.noreply.github.com>
2019-07-11 15:37:27 +0200

    Update PdfcreatorApplication.java

--- src/main/java/pl/ambroziak/pdfcreator/PdfcreatorApplication.java
+++ src/main/java/pl/ambroziak/pdfcreator/PdfcreatorApplication.java
@@ -12,9 +12,9 @@ public class PdfcreatorApplication {
     }
     public static void test() throws Exception {
         // TEST DATA
-        String dataXmlPath = "C:\\Users\\DELL\\Desktop\\Projekt_pdf\\test.xml";
-        String xslFilePath = "C:\\Users\\DELL\\Desktop\\Projekt_pdf\\test.xsl";
-        String path = "C:\\Users\\DELL\\Desktop\\Projekt_pdf\\";
+        String dataXmlPath = "C:\\Users\\USER\\test.xml";
+        String xslFilePath = "C:\\Users\\USER\\test.xsl";
+        String path = "C:\\Users\\USER\\";
         PDFCreator pdfCreator = new PDFCreator();
         pdfCreator.convertToPDF(dataXmlPath, xslFilePath, path);
     }
